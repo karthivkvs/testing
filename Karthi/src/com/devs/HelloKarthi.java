3e08c38a9f295207d5a7f833d0bb43fdf57bc218
HPE-EMB <HPE-EMB@HPE-EMB-PC>
2019-04-07 11:19:27 +0530

    My First commit

---
+++ Karthi/src/com/devs/HelloKarthi.java
@@ -0,0 +1,13 @@
+package com.devs;
+
+public class HelloKarthi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello java ");
+	}
+
+}
